9bd3e7323d595a1fbc6087f428a385bb1f2b14a5
real_brother <156147862+real-brother-ssafy@users.noreply.github.com>
2024-03-17 19:54:33 +0900

    123

---
+++ real-brother/week_08/src/BOJ_15685_드래곤커브/Main.java
@@ -0,0 +1,13 @@
+package BOJ_15685_드래곤커브;
+
+import java.util.Scanner;
+
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+	static int n;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
