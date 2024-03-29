b3f3ae9ced889d784289aef87311f474fe36d619
real-brother-ssafy <hjhassa@gmail.com>
2024-03-29 17:09:18 +0900

    week10 init

---
+++ real-brother/week_10/src/BOJ_1197_최소스패닝트리/Main.java
@@ -0,0 +1,11 @@
+package BOJ_1197_최소스패닝트리;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
