1174c12c8fee673f579627e4208b60ee7ff792dc
real-brother-ssafy <hjhassa@gmail.com>
2024-04-01 17:23:33 +0900

    캐슬디펜스 완료

--- real-brother/week_10/src/BOJ_17281_야구/Main.java
+++ real-brother/week_10/src/BOJ_17281_야구/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	static int[] fieldStatus = { 0, 0, 0 }; // 1루, 2루, 3루 상태
 	static int out = 0, maxTeamScore = 0;
 
-	static int[] hitterOrder = { 1, 2, 3, 5, 6, 7, 8, 9 };
+	static int[] hitterOrder = { 2, 3, 4, 5, 6, 7, 8, 9 };
 	static int[] permutation = new int[9];
 	static boolean[] visited = new boolean[8];
 
@@ -24,23 +24,23 @@ public class Main {
 			}
 		}
 
+		permutation[3] = 1;
 		settingHitterOrder(0, 0);
 
 		System.out.println(maxTeamScore);
 	}
 
 	// 순열로 타자 순서 구하기
-	// 맨앞에 4번타자 두고, 그 뒤 순서를 순열로 구하기
+	// 4번째 순서에 1번타자 두고, 나머지 순서를 순열로 구하기
 	// 8! = 약 4만
 	private static void settingHitterOrder(int start, int depth) {
 		if (depth == 9) {
 			simulateBaseballGame();
 			return;
 		}
-		if (depth == 0) {
-			permutation[0] = 4;
+		if (depth == 3) {
 			settingHitterOrder(0, depth + 1);
-
+			return;
 		}
 		for (int i = 0; i < 8; i++) {
 			int order = hitterOrder[i];
@@ -60,7 +60,7 @@ public class Main {
 	    for (int i = 0; i < 9; i++) {
 	        applyOrder[i] -= 1;
 	    }
-        int startHitter = 0; // startHitter를 이닝마다 초기화
+        int startHitter = 0; // startHitter를 맨 처음에 초기화
         for (int i = 0; i < innings; i++) {
         	int[] hitterStatus = new int[9];
         	for (int j = 0; j < hitterStatus.length; j++) {
@@ -77,63 +77,48 @@ public class Main {
 	                    case 0:
 	                        out++;
 	                        startHitter = o + 1;
-	                        if (out == 3) {
-	                            if (startHitter == 9) startHitter -= 9;
-	                            break inningOut;
-	                        }
 	                        break;
 	                    case 1:
-	                        for (int j = 2; j >= 0; j--) {
-	                            if (j == 2 && fieldStatus[2] == 1) {
-	                                gameScore++;
-	                                fieldStatus[2] = 0;
-	                            } else if (fieldStatus[j] == 1) {
-	                                fieldStatus[j + 1] = 1;
-	                                fieldStatus[j] = 0;
-	                            }
-	                        }
-	                        fieldStatus[0] = 1;
+	                    	if (fieldStatus[2] == 1) gameScore++;
+	                    	fieldStatus[2] = fieldStatus[1];
+	                    	fieldStatus[1] = fieldStatus[0];
+	                    	fieldStatus[0] = 1;
 	                        break;
 	                    case 2:
-	                        for (int j = 2; j >= 0; j--) {
-	                            if (j == 2 && fieldStatus[j] == 1) {
-	                                gameScore++;
-	                                fieldStatus[2] = 0;
-	                            } else if (j == 1 && fieldStatus[j] == 1) {
-	                                gameScore++;
-	                                fieldStatus[1] = 0;
-	                            } else if (fieldStatus[j] == 1) {
-	                                fieldStatus[j + 2] = 1;
-	                                fieldStatus[j] = 0;
-	                            }
-	                        }
-	                        fieldStatus[1] = 1;
+	                    	if (fieldStatus[2] == 1) gameScore++;
+	                    	if (fieldStatus[1] == 1) gameScore++;
+	                    	fieldStatus[2] = fieldStatus[0];
+	                    	fieldStatus[1] = 1;
+	                    	fieldStatus[0] = 0;
 	                        break;
 	                    case 3:
-	                        for (int j = 2; j >= 0; j--) {
-	                            if (fieldStatus[j] == 1) {
-	                                gameScore++;
-	                                fieldStatus[j] = 0;
-	                            }
-	                        }
-	                        fieldStatus[2] = 1;
+	                    	if (fieldStatus[2] == 1) gameScore++;
+	                    	if (fieldStatus[1] == 1) gameScore++;
+	                    	if (fieldStatus[0] == 1) gameScore++;
+	                    	fieldStatus[2] = 1;
+	                    	fieldStatus[1] = 0;
+	                    	fieldStatus[0] = 0;
 	                        break;
 	                    case 4:
-	                        int count = 1;
-	                        for (int j = 0; j < 3; j++) {
-	                            if (fieldStatus[j] == 1)
-	                                count++;
-	                        }
-	                        gameScore += count;
-	                        fieldStatus = new int[]{0, 0, 0};
+	                    	if (fieldStatus[2] == 1) gameScore++;
+	                    	if (fieldStatus[1] == 1) gameScore++;
+	                    	if (fieldStatus[0] == 1) gameScore++;
+	                    	gameScore++;
+	                    	fieldStatus[2] = 0;
+	                    	fieldStatus[1] = 0;
+	                    	fieldStatus[0] = 0;
 	                        break;
 	                }
+	                if (out == 3) {
+	                    if (startHitter == 9) startHitter -= 9;
+	                    break inningOut;
+	                }
 	            }
+	            
 	            startHitter = 0; // 이닝 종료 후 다음 이닝을 위해 초기화
 	        }
-	        if (maxTeamScore < gameScore)
-	            maxTeamScore = gameScore;
 	    }
+        maxTeamScore = Math.max(maxTeamScore, gameScore);
 	}
 
 }
