a0c61ecb770d954fa0f7a0223f4ae86b5d283077
wngud1225 <swwngud1225@gmail.com>
2024-07-08 22:32:39 +0900

    Sovled: BOJ_1502_내리막 길

--- wngud/week_21/1502_내리막 길/Main.java
+++ wngud/week_21/1502_내리막 길/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    static int[][] matrix, visited, checked;
+    static int[][] matrix, dp;
     static int N, M, answer;
 
     static int[] dr = {-1, 1, 0, 0};
@@ -15,68 +15,48 @@ public class Main {
         N = sc.nextInt();
         M = sc.nextInt();
 
+        // matrix 입력, dp 초기화 -1 -> 메모제이션 유무 파악
         matrix = new int[N][M];
+        dp = new int[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 int num = sc.nextInt();
                 matrix[i][j] = num;
+                dp[i][j] = -1;
             }
         }
-
+        
         // dfs
-        answer = 0;
-        visited = new int[N][M];
-        checked = new int[N][M];
-
-        visited[0][0] = 1;
-        dfs(0, 0, matrix[0][0]);
+        dfs(0, 0);
 
         // 정답 출력하기
-        System.out.println(answer);
+        System.out.println(dp[0][0]);
     }
 
-    public static  void dfs(int r, int c, int prev) {
 
-        if (r == N-1 && c == M-1) {
-            answer++;
+    public static int dfs(int r, int c) {
 
-            // visted 반영
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (visited[i][j] == 0) continue;
-                    checked[i][j] = visited[i][j];
-                }
-            }
-    
-            return;
-        }
+        // 목적지 도착
+        if (r == N-1 && c == M-1) return 1;
+
+        // 1) 이미 방문한 경우
+        if (dp[r][c] != -1) return dp[r][c];
+
+
+        // 2) 방문하지 않은 경우 (-1)
+        dp[r][c] = 0; // 방문체크
 
         for (int m = 0; m < 4; m++) {
             int nr = r + dr[m];
             int nc = c + dc[m];
 
             if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-            if (matrix[nr][nc] >= prev) continue;
+            if (matrix[r][c] <= matrix[nr][nc]) continue;
 
-            // 이전에 길이 있던 경우
-            if (checked[nr][nc] == 1) {
-                answer++;
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < M; j++) {
-                        if (visited[i][j] == 0) continue;
-                        checked[i][j] = visited[i][j];
-                    }
-                }
-
-                continue;
-            }
-
-            visited[nr][nc] = 1;
-            dfs(nr, nc, matrix[nr][nc]);
-            visited[nr][nc] = 0;
+            // 현재 값이 더 큰 경우
+            dp[r][c] += dfs(nr, nc); // 재귀
         }
 
-        
-
+        return dp[r][c];
     }
 }
