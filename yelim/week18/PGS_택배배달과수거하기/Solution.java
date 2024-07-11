c439e5d183b083222a6c13d3bb1e10104b84296d
yelim99 <156084622+yelim99@users.noreply.github.com>
2024-07-11 22:52:42 +0900

    Solved: PGS_택배배달과수거하기

---
+++ yelim/week18/PGS_택배배달과수거하기/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*
+* 먼 거리부터 해결..
+* 
+*/
+
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        
+        int d = 0;
+        int p = 0;
+        
+        for(int i=n-1; i>=0; i--) {
+            d -= deliveries[i];
+            p -= pickups[i];
+            
+            while (d<0 || p<0) {
+                d += cap;
+                p += cap;
+                
+                answer += (i+1) * 2;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
